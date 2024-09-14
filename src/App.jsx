486215dc74f238d6f8d70950ff271c2cdf2890f8
nicksemk <pietronicollas34conta2@gmail.com>
2024-09-14 07:44:01 -0300

    car-projrtc

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,18 @@ function App() {
   
  const cars = [
   {id:1, brand:"BMW",model:"320i",color: "Azul", km:0},
-  {id:2, brand:"Porsche",model:"Panamera",color: "Azul", km:0},
-  {id:3, brand:"Audi",model:"A6",color: "Azul", km:0},
-  {id:4, brand:"Jeep",model:"Compass",color: "Azul", km:2452},
+  {id:2, brand:"Porsche",model:"PANAMERA",color: "Vermelha", km:0},
+  {id:3, brand:"Audi",model:"A6",color: "Branco", km:0},
+  {id:5, brand:"Jeep",model:"COMPASS",color: "Vermelho", km:2342},
+  {id:6, brand:"Honda",model:"CIVIC",color: "Preto", km:452},
+  {id:7, brand:"Chevrovel",model:"S10 HIGH COUNTRY",color: "Azul", km:24342},
  ];
 
   return (
     <>
     <div>
       <h1>Car Project!</h1>
+      <div className="car_conteiner">
       {cars.map((car)=> (
         <CarComponents
         key={car.id}
@@ -26,7 +29,8 @@ function App() {
         km={car.km}
         />
       ))}
-    </div>   
+    </div>  
+    </div> 
     </>
   )
 }
--- src/components/CarComponents.jsx
+++ src/components/CarComponents.jsx
@@ -3,7 +3,7 @@ import styles from  './CarComponents.module.css';
 const CarComponents = ({brand, color, model, km}) => {
   return (
     <div className={styles.CarComponents}>
-        <h2>Marca: {brand}</h2>
+        <h2> {brand}</h2>
         <h4>Cor: {color}</h4>
         <h4>Modelo: {model}</h4>
         <h4>km: {km}</h4>
